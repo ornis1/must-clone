dbd77ebef02c2f84efaccfcc92369d5b8d3aefb6
ornis1 <fastmarat23@gmail.com>
2020-11-04 01:19:52 +0300

    Youtube component

---
+++ components/Youtube.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .youtube
+    iframe(:src="url" allowfullscreen)
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class Youtube extends Vue {
+  @Prop({ default: '' }) readonly yid!: string
+
+  get url() {
+    const query = '?rel=0&showinfo=0&autoplay=1'
+    return `https://www.youtube.com/embed/${this.yid}${query}`
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.youtube {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  padding-top: 62.5%;
+  width: 100%;
+  height: 0;
+  background-color: #000;
+
+  iframe {
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: block;
+    border: none;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
